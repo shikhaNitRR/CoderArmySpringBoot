49d4c2f3e51e5d87b2e83e1a6073cebb44499294
Shikha <shikha.sri96@gmail.com>
2026-06-26 23:32:23 +0530

    Added notification folder

---
+++ src/main/java/notification/EmailService.java
@@ -0,0 +1,6 @@
+package notification;
+public class EmailService {
+    public void sendEmailNotification() {
+        System.out.println(" Mail Sent To the Customer");
+    }
+}
---
+++ src/main/java/notification/SmsService.java
@@ -0,0 +1,6 @@
+package notification;
+public class SmsService {
+    public void sendSMSNotification() {
+        System.out.println(" SMS Sent To The Customer");
+    }
+}
